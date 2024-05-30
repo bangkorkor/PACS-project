5109071d47514983fe9a09da3393a617bd892cd1
Ask Kvarven <ask.kvarven@icloud.com>
2024-05-30 13:13:16 +0200

    GetPot for main added

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,20 @@
 #include "phys_mod.hpp"
 #include "num_mod.hpp"
 #include "mixer.hpp"
+#include "GetPot"
 
-int main()
-{
-    size_t number_of_phys_mods = 2;
-    size_t number_of_num_mods = 40;
+int main(int argc, char **argv) {
+    GetPot command_line(argc, argv);
+
+    size_t number_of_phys_mods = command_line("np", 2);
+    size_t number_of_num_mods = command_line("nn", 40);
     mixer m(number_of_phys_mods, number_of_num_mods);
 
-    double t_0 = 293;
-    double p_0 = 0;
-    double RPM = 100;
-    double Q = 150;
-    m.set_parameters(t_0, p_0, RPM, Q);
+    double t0 = command_line("t0",293);
+    double p0 = command_line("p0",0);
+    double RPM = command_line("RPM",100);
+    double Q = command_line("Q",150);
+    m.set_parameters(t0, p0, RPM, Q);
 
     m.simulate_mixer();
     m.print_mixer();
